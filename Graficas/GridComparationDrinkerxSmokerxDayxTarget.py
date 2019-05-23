1c8f12dc6fdf88fc2e3190c152c4b3c4fe1e8678
Albeiro <albeiroespitiasierra@gmail.com>
2019-05-23 02:42:52 -0500

    Agregando grafica de grillas

--- Graficas/GridComparationDrinkerxSmokerxDayxTarget.py
+++ Graficas/GridComparationDrinkerxSmokerxDayxTarget.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from collections import Counter
-
 
 namesColumn = ["ID", "Reason for absence","Month of absence", "Day of the week", "Seasons", "Transportation expense", "Distance from Residence to Work", "Service time", "Age", "Work load Average/day", "Hit target", "Disciplinary failure", "Education", "Son", "Social drinker", "Social smoker", "Pet", "Weight","Height","Body mass index","Absenteeism time in hours"]
 
@@ -13,5 +11,7 @@ dataframe = dataframe[dataframe["Absenteeism time in hours"] != 0]
 X = dataframe.iloc[:, 0:20]
 y = pd.DataFrame(dataframe["Absenteeism time in hours"])
 
-sns.jointplot(x="Absenteeism time in hours", y="Month of absence", data=dataframe, kind="hex")
+
+sn = sns.FacetGrid(col="Social smoker", data=dataframe, hue="Social drinker", palette="Set1")
+(sn.map(plt.scatter, "Day of the week", "Absenteeism time in hours", edgecolor="w").add_legend())
 plt.show()
