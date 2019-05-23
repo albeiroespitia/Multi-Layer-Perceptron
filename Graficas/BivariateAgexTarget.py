feb198511d5cea5a9d0011b282d013624ae3de29
Albeiro <albeiroespitiasierra@gmail.com>
2019-05-23 01:37:15 -0500

    Agregando mas graficas con seaborn

--- Graficas/BivariateAgexTarget.py
+++ Graficas/BivariateAgexTarget.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from collections import Counter
+
 
 namesColumn = ["ID", "Reason for absence","Month of absence", "Day of the week", "Seasons", "Transportation expense", "Distance from Residence to Work", "Service time", "Age", "Work load Average/day ", "Hit target", "Disciplinary failure", "Education", "Son", "Social drinker", "Social smoker", "Pet", "Weight","Height","Body mass index","Absenteeism time in hours"]
 
 dataframe = pd.read_csv('./../Absenteeism_at_work.csv', names=namesColumn, sep=",",header=0)
 
+dataframe = dataframe[dataframe["Absenteeism time in hours"] != 0]
+
 X = dataframe.iloc[:, 0:20]
 y = pd.DataFrame(dataframe["Absenteeism time in hours"])
+print(Counter(y.values.ravel()))
 
-sns.jointplot(x='Absenteeism time in hours',y='Age',data=dataframe)
-
+sns.jointplot(x="Absenteeism time in hours", y="Age", color="g" , data=dataframe, kind="scatter")
 plt.show()
